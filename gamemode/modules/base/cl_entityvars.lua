e1e5fc8005ef744ca4028db9a2deafdc297f3c73
FPtje <fpeijnenburg@gmail.com>
2014-01-19 19:31:54 +0100

    Debugging playervars

--- gamemode/modules/base/cl_entityvars.lua
+++ gamemode/modules/base/cl_entityvars.lua
@@ -17,11 +17,12 @@ local function RetrievePlayerVar(entIndex, var, value, tries)
 	-- In this case, chances are huge that this player will become valid.
 	if not IsValid(ply) then
 		if (tries or 0) >= 5 then return end
-
+		print("DARKRPDEBUG", "retrying DarkRPVar for ", entIndex, var, tries)
 		timer.Simple(0.5, function() RetrievePlayerVar(entIndex, var, value, (tries or 0) + 1) end)
 		return
 	end
 
+	print("DARKRPDEBUG", "Retrieving single DarkRPVar succesful for ", entIndex, var, tries)
 	ply.DarkRPVars = ply.DarkRPVars or {}
 
 	hook.Call("DarkRPVarChanged", nil, ply, var, ply.DarkRPVars[var], value)
@@ -37,6 +38,7 @@ local function doRetrieve()
 	local var = net.ReadString()
 	local valueType = net.ReadUInt(8)
 	local value = net.ReadType(valueType)
+	print("DARKRPDEBUG", "RETRIEVING DARKRPVAR", entIndex, Entity(entIndex), var, valueType, value)
 
 	RetrievePlayerVar(entIndex, var, value)
 end
@@ -47,10 +49,13 @@ Initialize the DarkRPVars at the start of the game
 ---------------------------------------------------------------------------*/
 local function InitializeDarkRPVars(len)
 	local vars = net.ReadTable()
+	print("DARKRPDEBUG INIT VARS", "A = VARS", vars)
+	A = A or {}
+	table.insert(A, vars)
 
 	if not vars then return end
 	for k,v in pairs(vars) do
-		if not IsValid(k) then continue end
+		if not IsValid(k) then print("DARKRPDEBUG", "PLAYER NOT VALID!", k, v) continue end
 		k.DarkRPVars = k.DarkRPVars or {}
 
 		-- Merge the tables
@@ -67,12 +72,16 @@ Request the DarkRPVars
 timer.Simple(1, fn.Curry(RunConsoleCommand, 2)("_sendDarkRPvars"))
 
 timer.Create("DarkRPCheckifitcamethrough", 15, 0, function()
+	print("DARKRPDEBUG", "CHECKING CAME THROUGH")
 	for k,v in pairs(player.GetAll()) do
 		if v.DarkRPVars and v:getDarkRPVar("rpname") then continue end
+		print("DARKRPDEBUG", "CHECKING CAME THROUGH", "NOPE DID NOT COME THROUGH")
 
 		RunConsoleCommand("_sendDarkRPvars")
 		return
 	end
+
+	print("DARKRPDEBUG", "CHECKING CAME THROUGH", "YES DID COME THROUGH")
 
 	timer.Destroy("DarkRPCheckifitcamethrough")
 end)
